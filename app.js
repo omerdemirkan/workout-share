19554da591b0b81a4b1a4a26a4a2f15c55e2c567
Omer Demirkan <omerfarukpiano@gmail.com>
2019-12-26 11:05:03 -0800

    heroku attempt 2, path.resolve() replaced by path.join()

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('front-end/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'front-end', 'build', 'index.html'));
     });
 }
  
