7e6995565828aad2753406530f21f29bc211a12f
Omer Demirkan <omerfarukpiano@gmail.com>
2019-12-28 18:46:20 -0800

    fix linkdin link

--- front-end/src/components/UI/Footer/Footer.js
+++ front-end/src/components/UI/Footer/Footer.js
@@ -10,7 +10,7 @@ const footer = () => {
     return <div className={classes.Footer}>
         <h3 className={classes.Header}>Omer Demirkan, {new Date().getFullYear()}</h3>
         <div className={classes.LinkBox}>
-            <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile" target="_blank"><LinkedInIcon className={classes.LinkIcon}/></a>
+            <a href="www.linkedin.com/in/omer-demirkan" target="_blank"><LinkedInIcon className={classes.LinkIcon}/></a>
             <a href="https://github.com/omerdemirkan" target="_blank"><GitHubIcon className={classes.LinkIcon}/></a>
             <a href="mailto:omerfarukpiano@gmail.com"><MailOutlineIcon className={classes.LinkIcon}/></a>
         </div>
